0670cc099af376c97c06c918ac518b7d4ce85ce9
Greg <g.rutherford10@gmail.com>
2020-02-14 14:16:37 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import melbourneCup from './images/melbourne_cup.jpg';
+import melbourneCupSnap from './images/melbourne-cup-snap.jpg';
+import dayOut from './images/day-out.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Happy Valentines Day !</h1>
       </header>
+      <main>
+        <section className="description">
+          <p>
+            Instead of wasting paper on a Valentines card I decided to
+            make you this ! I Hope you like it
+          </p>
+          <p id="love">Love You !</p>
+        </section>
+        <section className="pictures">
+          <div className="picture-grid">
+            <img src={melbourneCup} />
+            <img src={dayOut} />
+            <img src={melbourneCupSnap} />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
